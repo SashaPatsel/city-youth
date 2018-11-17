3371d9023816dd13e89e4a128c5c3403f80faa12
Sasha Patsel <sasha8patsel@gmail.com>
2018-11-17 11:38:47 -0800

    add home page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+class Home extends Component {
+  state = {
+
+  }
+
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="home"></div>
+    )
+  }
+}
